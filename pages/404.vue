da5525ebf102c6c346362584d7bccb2df75f25ad
Wayne Gibson <wayne.gibson@spacecomx.com>
2022-09-01 01:58:59 -0400

    feat: added additional pages

--- pages/404.vue
+++ pages/404.vue
@@ -5,10 +5,10 @@
     <div class="flex justify-center items-center h-screen">
       <div class="flex-col text-center">
         <h1 class="font-display text-6xl font-black text-gray-500">
-          Hello World!
+          404
         </h1>
         <p class="font-sans text-xl font-normal text-gray-500 mt-3">
-          This is a demo page V1.
+          This is a 404 page.
         </p>
       </div>
     </div>
